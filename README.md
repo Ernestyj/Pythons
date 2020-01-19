1a07eff4c6ea85e7fdd1a44973ae81e2976d1ed4
Yang, Jian <jyang2@paypal.com>
2020-01-19 18:20:38 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Pythons
+
+Environment: Python 3
+
+Preparation:
+
+1. prepare directories and configuration file
+   ```bash
+   cd PROJECT_ROOT
+   mkdir external
+   touch conf.jsoon
---
+++ service/playground/test/test_example.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from service.settings.settings import *
+
+
+class TestExample(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("**************************************** setUpClass ****************************************")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("************************************** tearDownClass ***************************************")
+
+    def setUp(self):
+        print("****** setUp *******")
+
+    def tearDown(self):
+        print("***** tearDown *****")
+
+    def _example(self):
+        print("This is a test example.")
+        print(TIMEZONE)
+
+        # df_time_used = instance.query_sample()
+        # print(df_time_used)
